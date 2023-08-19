964884289544b2ec105ddc17cfb4d0381d3a0676
p21nc3 <pp4768@srmist.edu.in>
2023-08-19 23:45:58 +0530

    rustc

---
+++ target/debug/deps/thecompiler-2d4491f6d2b8cc1a.d
@@ -0,0 +1,5 @@
+/mnt/d/bhardwaj/thecompiler/target/debug/deps/thecompiler-2d4491f6d2b8cc1a: src/main.rs
+
+/mnt/d/bhardwaj/thecompiler/target/debug/deps/thecompiler-2d4491f6d2b8cc1a.d: src/main.rs
+
+src/main.rs:
